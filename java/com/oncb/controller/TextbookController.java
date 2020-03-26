0ce35ed7e0808b00400a6d3d13cbf2fafab79962
dearother <dearother@gmail.com>
2020-03-26 15:10:08 +0800

    1.0.5

--- java/com/oncb/controller/TextbookController.java
+++ java/com/oncb/controller/TextbookController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
